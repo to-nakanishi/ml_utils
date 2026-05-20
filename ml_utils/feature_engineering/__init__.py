60b1a6eee50ea6b20f166069660805cc4cd6bb92
Tomomi Nakanishi <z11779537@gmail.com>
2026-05-20 20:47:11 +0900

    chore: export make_composite_features

--- ml_utils/feature_engineering/__init__.py
+++ ml_utils/feature_engineering/__init__.py
@@ -2,5 +2,11 @@
 
 from .memory import downcast_numeric
 from .target_encoding import target_encode_oof, diagnose_target_encoding
+from .composite import make_composite_features
 
-__all__ = ['downcast_numeric', 'target_encode_oof', 'diagnose_target_encoding']
+__all__ = [
+    'downcast_numeric',
+    'target_encode_oof',
+    'diagnose_target_encoding',
+    'make_composite_features',
+]
