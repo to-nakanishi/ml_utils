0d9003ed152fb5e3a03d4ed76b4347e0041a5a33
Tomomi Nakanishi <z11779537@gmail.com>
2026-05-20 20:20:07 +0900

    chore: export diagnose_target_encoding

--- ml_utils/feature_engineering/__init__.py
+++ ml_utils/feature_engineering/__init__.py
@@ -1,6 +1,6 @@
 """Feature engineering utilities."""
 
 from .memory import downcast_numeric
-from .target_encoding import target_encode_oof
+from .target_encoding import target_encode_oof, diagnose_target_encoding
 
-__all__ = ['downcast_numeric', 'target_encode_oof']
+__all__ = ['downcast_numeric', 'target_encode_oof', 'diagnose_target_encoding']
