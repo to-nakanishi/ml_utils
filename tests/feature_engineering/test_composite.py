1901ce9aeb37b077240dc6a9d26c6e6d14661d9a
Tomomi Nakanishi <z11779537@gmail.com>
2026-05-25 11:21:28 +0900

    style: fix import order in test files

--- tests/feature_engineering/test_composite.py
+++ tests/feature_engineering/test_composite.py
@@ -1,7 +1,7 @@
 """make_composite_features のテスト."""
-import pytest
 import numpy as np
 import pandas as pd
+import pytest
 from ml_utils.feature_engineering.composite import make_composite_features
 
 
