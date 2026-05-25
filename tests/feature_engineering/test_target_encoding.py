0e2b096af9221304bc5769a5353857ed74e698a3
Tomomi Nakanishi <z11779537@gmail.com>
2026-05-25 16:46:29 +0900

    style: sort imports in test files

--- tests/feature_engineering/test_target_encoding.py
+++ tests/feature_engineering/test_target_encoding.py
@@ -1,10 +1,11 @@
 """target_encode_oof のテスト."""
-import pytest
 import numpy as np
 import pandas as pd
+import pytest
+
 from ml_utils.feature_engineering.target_encoding import (
-    target_encode_oof,
     diagnose_target_encoding,
+    target_encode_oof,
 )
 
 
